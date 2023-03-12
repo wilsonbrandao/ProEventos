e751a5b668a75134d99c33df7b839b6fcce542fb
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-03-12 17:57:35 -0300

    feat: create eventos component

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filterBy: string) : any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos
+      .filter((event: { tema: string; local: string; }) => {
+        return event.tema.toLocaleLowerCase().indexOf(filterBy) !== -1
+          || event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    });
+  }
+
 
   constructor(private http: HttpClient) { }
 
@@ -19,8 +45,16 @@ export class EventosComponent implements OnInit {
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos')
       .subscribe(
-        response => this.eventos = response,
+        response => {
+          this.eventos = response;
+          this.eventosFiltrados = this.eventos;
+        },
         error => console.log(error)
       );
   }
+
+  handlerImg(): void{
+    this.showImg = !this.showImg;
+  }
+
 }
